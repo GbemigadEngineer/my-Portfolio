cfe28243e68f1dcf9e6b733a5203c127004b54ee
GbemigadEngineer <samueloduwolegbolahan@gmail.com>
2024-12-17 09:49:49 +0100

    Updated my javascript with what i already did locally!

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const learnMoreLink = document.getElementById("learn-more");
+
+const aboutDetails = document.querySelector(".about-details");
+
+const showLessLink = document.getElementById("show-less");
+
+// Event listener to the about me section
+
+learnMoreLink.addEventListener("click", function (event) {
+  event.preventDefault();
+  learnMoreLink.style.opacity = 0;
+  aboutDetails.style.display = "flex";
+  showLessLink.style.opacity = 100;
+});
+
+showLessLink.addEventListener("click", function (event) {
+  event.preventDefault();
+  showLessLink.style.opacity = 0;
+  aboutDetails.style.display = "none";
+  learnMoreLink.style.opacity = 100;
+});
