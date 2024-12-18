a8b5a1227d9334b49c98f68e5598f36f9bc82d23
GbemigadEngineer <samueloduwolegbolahan@gmail.com>
2024-12-18 13:26:09 +0100

    worked on the gallery section and built the slider component

--- js/script.js
+++ js/script.js
@@ -28,3 +28,82 @@ document.querySelector(
   "footer p"
 ).textContent = `© ${year} Oduwole Oluwagbemiga. All rights reserved.`;
 
+// The Gallery section
+// Implementing smooth scrolling
+
+const slides = document.querySelectorAll(".slide");
+const btnLeft = document.querySelector(".slider__btn--left");
+const btnRight = document.querySelector(".slider__btn--right");
+const dotContainer = document.querySelector(".dots");
+
+let currentSlide = 0;
+const maxSlide = slides.length;
+
+const createDots = function () {
+  slides.forEach((_, index) => {
+    dotContainer.insertAdjacentHTML(
+      "beforeend",
+      `<button class="dots__dot" data-slide=${index}></button>`
+    );
+  });
+};
+
+const activateDot = function (slide) {
+  document
+    .querySelectorAll(".dots__dot")
+    .forEach((dot) => dot.classList.remove("dots__dot--active"));
+
+  document
+    .querySelector(`.dots__dot[data-slide = "${slide}"]`)
+    .classList.add("dots__dot--active");
+};
+
+const goToSlide = function (slide) {
+  slides.forEach((slides, index) => {
+    slides.style.transform = `translateX(${100 * (index - slide)}%)`;
+  });
+};
+
+const nextSlide = function () {
+  if (currentSlide === maxSlide - 1) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+
+  goToSlide(currentSlide);
+  activateDot(currentSlide);
+};
+const previousSlide = function () {
+  if (currentSlide === 0) {
+    currentSlide = maxSlide - 1;
+  } else {
+    currentSlide--;
+  }
+  goToSlide(currentSlide);
+  activateDot(currentSlide);
+};
+
+const init = function () {
+  goToSlide(0);
+  createDots();
+  activateDot(0);
+};
+init();
+//Event Handlers
+btnRight.addEventListener("click", nextSlide);
+
+btnLeft.addEventListener("click", previousSlide);
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") previousSlide();
+  event.key === "ArrowRight" && nextSlide();
+});
+
+dotContainer.addEventListener("click", function (event) {
+  if (event.target.classList.contains("dots__dot")) {
+    const { slide } = event.target.dataset;
+    goToSlide(slide);
+    activateDot(slide);
+  }
+});
